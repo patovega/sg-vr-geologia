1b3ccf1be810462c31db228d8a546ec22bfc8719
patovega <patricio.vega1925@gmail.com>
2025-07-28 10:40:18 -0400

    first commit

--- js/scenes.js
+++ js/scenes.js
@@ -1,26 +1,26 @@
 const scenes = [
     {
-        id: '1_',
+        id: '1',
         title: '1era estacion',
         filename: './images/1.jpg',
         
     },
     {
-        id: '2_',  
+        id: '2',  
         title: '2da estacion',
         filename: './images/2.jpg',
         
     },
     {
-        id: '3_',
+        id: '3',
         title: '3ra estacion',
         filename: './images/3.jpg',
         
     },
     {
-        id: '4_fotografia',
+        id: '4',
         title: 'Fotografia',
-        filename: './images/4_fotografia.jpg',
+        filename: './images/4.jpg',
         
     } 
     
